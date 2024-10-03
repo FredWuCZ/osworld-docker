99d28e0e81a5fe578b22ad7fe1b0933f663fe2ce
FredWuCZ <fredwucz@outlook.com>
2024-10-03 16:10:15 +0800

    Update

--- src/install.sh
+++ src/install.sh
@@ -168,8 +168,8 @@ findBackingFile() {
   local ext="$1"
   local file
 
-  file=$(find / -maxdepth 1 -type f -iname "Ubuntu.$ext" | head -n 1)
-  [ ! -s "$file" ] && file=$(find "$STORAGE" -maxdepth 1 -type f -iname "Ubuntu.$ext" | head -n 1)
+  file=$(find / -maxdepth 1 -type f -iname "System.$ext" | head -n 1)
+  [ ! -s "$file" ] && file=$(find "$STORAGE" -maxdepth 1 -type f -iname "System.$ext" | head -n 1)
   detectType "$file" && return 0
 
   return 1
@@ -187,8 +187,8 @@ findFile() {
   return 1
 }
  
-findBackingFile "img" && qemu-img create -f qcow2 -b /Ubuntu.img -F qcow2 /boot.img
-findBackingFile "qcow2" && qemu-img create -f qcow2 -b /Ubuntu.qcow2 -F qcow2 /boot.qcow2
+findBackingFile "img" && qemu-img create -f qcow2 -b /System.img -F qcow2 /boot.img
+findBackingFile "qcow2" && qemu-img create -f qcow2 -b /System.qcow2 -F qcow2 /boot.qcow2
 
 findFile "iso" && return 0
 findFile "img" && return 0
